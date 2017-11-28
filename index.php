cad1d1f07bf6225ddccaab0c15cd058607b110b9
Paul Farr <farrp2011@live.com>
2017-11-27 16:06:10 -0800

    First Commit--- testing commiting to github

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Paul is cool and you need to give him money
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            print("Hola!");
+        ?>
+    </body>
+</html>
